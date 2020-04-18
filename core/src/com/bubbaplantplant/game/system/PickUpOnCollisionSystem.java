868e586a4729e662c04a35f7189a1666351bd776
Johan Nilsson Hansen <johan.hansen@meltwater.com>
2020-04-18 20:30:53 +0200

    Picks up bucket

--- core/src/com/bubbaplantplant/game/system/PickUpOnCollisionSystem.java
+++ core/src/com/bubbaplantplant/game/system/PickUpOnCollisionSystem.java
@@ -3,9 +3,11 @@ package com.bubbaplantplant.game.system;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.EntitySystem;
 import com.badlogic.ashley.core.Family;
+import com.badlogic.gdx.math.Vector3;
 import com.bubbaplantplant.game.component.CollisionComponent;
 import com.bubbaplantplant.game.component.PickUpableComponent;
 import com.bubbaplantplant.game.component.PlayerComponent;
+import com.bubbaplantplant.game.component.PositionComponent;
 
 import java.util.List;
 
@@ -13,11 +15,12 @@ public class PickUpOnCollisionSystem extends EntitySystem {
     @Override
     public void update(float deltaTime) {
         Entity playerEntity = getEngine().getEntitiesFor(Family.all(PlayerComponent.class).get()).first();
+        PositionComponent playerPosition = playerEntity.getComponent(PositionComponent.class);
         List<Entity> collidingWith = playerEntity.getComponent(CollisionComponent.class).getCollidingWith();
 
         for (Entity entity : collidingWith) {
             if (entity.getComponent(PickUpableComponent.class) != null) {
-                System.out.println("Will pick this item up");
+                entity.getComponent(PositionComponent.class).setPosition(new Vector3(0.0f, 1.0f, 1.0f).add(playerPosition.getPosition()));
             }
         }
     }
